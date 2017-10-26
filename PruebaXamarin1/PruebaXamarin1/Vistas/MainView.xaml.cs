1f309e604de55071d2fca7f807d83d0fedae7a96
Karlichuss <carlossant791@hotmail.com>
2017-10-26 16:22:21 +0200

    Agregar archivos de proyecto.

---
+++ PruebaXamarin1/PruebaXamarin1/Vistas/MainView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PruebaXamarin1.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainView : ContentPage
+    {
+        // Variable contador, se inicializa a 1
+        int count = 1;
+
+        public MainView()
+        {
+            InitializeComponent();
+
+            MyButton.Clicked += (sender, args) =>
+            {
+                MyLabel.Text = String.Format("Se han hecho {0} clicks!", count++);
+            };
+        }
+    }
+}
